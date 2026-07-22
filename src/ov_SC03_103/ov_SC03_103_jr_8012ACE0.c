fbed540ef201f8cebccf061a64d35322a1daed19
Drew T <50529377+Druthulu@users.noreply.github.com>
2026-07-22 12:14:04 -0600

    feat(phase-29): func_80135888 sweep chunk 2 — 46/46 banked

--- src/ov_SC03_103/ov_SC03_103_jr_8012ACE0.c
+++ src/ov_SC03_103/ov_SC03_103_jr_8012ACE0.c
@@ -2951,6 +2951,3 @@ s16 func_80135480(void *param_1, s32 param_2, s16 *param_3, s16 *param_4)
     }
     return 1;
 }
-
-
-INCLUDE_ASM("asm/ov_SC03_103/nonmatchings/ov_SC03_103_jr_8012ACE0", func_80135888);
--- src/ov_SC04_018/ov_SC04_018_jr_8012ACE0.c
+++ src/ov_SC04_018/ov_SC04_018_jr_8012ACE0.c
@@ -2953,6 +2953,3 @@ s16 func_80135480(void *param_1, s32 param_2, s16 *param_3, s16 *param_4)
     }
     return 1;
 }
-
-
-INCLUDE_ASM("asm/ov_SC04_018/nonmatchings/ov_SC04_018_jr_8012ACE0", func_80135888);
