3bfcc117bc670393a34a43053ec0f3df0511bc6f
Drew T <50529377+Druthulu@users.noreply.github.com>
2026-07-24 02:03:25 -0600

    feat(decomp): grinder gate — +1 fns x0 propagated (fleet None%)

--- src/ov_SC01_006/ov_SC01_006_jr_8017C340.c
+++ src/ov_SC01_006/ov_SC01_006_jr_8017C340.c
@@ -3403,7 +3403,31 @@ INCLUDE_ASM("asm/ov_SC01_006/nonmatchings/ov_SC01_006_jr_8017C340", func_8018067
 
 INCLUDE_ASM("asm/ov_SC01_006/nonmatchings/ov_SC01_006_jr_8017C340", func_801806B8);
 
-INCLUDE_ASM("asm/ov_SC01_006/nonmatchings/ov_SC01_006_jr_8017C340", func_80180710);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+extern s16 func_8017DA9C(void);
+int func_80180710(void)
+{
+  int new_var;
+  new_var = 0x1;
+  new_var--;
+  return (((unsigned int) ((int (*)(void))func_8017DA9C)()) << 16) == new_var;
+  new_var++;
+}
 
 INCLUDE_ASM("asm/ov_SC01_006/nonmatchings/ov_SC01_006_jr_8017C340", func_80180734);
 
